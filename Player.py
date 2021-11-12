cf26d9f4896819375f4f3b0ffa967528239898ef
ShahriarMiah <85572689+ShahriarMiah@users.noreply.github.com>
2021-11-12 23:40:26 +0000

    Update Player.py

--- Player.py
+++ Player.py
@@ -17,9 +17,3 @@ class Player:
             self.X -= self.Vel
         if keys[self.Key2] and self.X < 371:
             self.X += self.Vel
-
-    def collision(self, X, velY):
-        if self.X < X < self.X + self.Width:
-            return True
-Player1 = Player(200, 699, 128, 4, pygame.K_LEFT, pygame.K_RIGHT)
-Player2 = Player(200, -44, 128, 4, pygame.K_a, pygame.K_d)
