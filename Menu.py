cf7b70e70d89334cb5a1a223b6166c69a5bfa138
ShahriarMiah <85572689+ShahriarMiah@users.noreply.github.com>
2021-11-12 23:41:07 +0000

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -13,22 +13,28 @@ class Menu:
         meow = True
         Clicked = False
         while meow:
-            self.Window.blit(self.font1.render("4 Player Pong!", True, (0, 0, 0)), [50, 50])
+            self.Window.blit(self.font1.render("4 Player Pong!", True, (255, 255, 255)), [50, 50])
             mx, my = pygame.mouse.get_pos()
-            option1 = pygame.draw.rect(self.Window, (0, 0, 0), (120, 200, 250, 75), 3)
-            option2 = pygame.draw.rect(self.Window, (0, 0, 0), (120, 300, 250, 75), 3)
-            option3 = pygame.draw.rect(self.Window, (0, 0, 0), (120, 400, 250, 75), 3)
-            self.Window.blit(self.font1.render("1 v 1", True, (0, 0, 0)), [175, 210])
-            self.Window.blit(self.font1.render("4-Player", True, (0, 0, 0)), [135, 310])
-            self.Window.blit(self.font1.render("Quit", True, (0, 0, 0)), [190, 410])
+            option1 = pygame.draw.rect(self.Window, (255, 255, 255), (120, 200, 250, 75), 3)
+            option2 = pygame.draw.rect(self.Window, (255, 255, 255), (120, 300, 250, 75), 3)
+            option3 = pygame.draw.rect(self.Window, (255, 255, 255), (120, 400, 250, 75), 3)
+            option4 = pygame.draw.rect(self.Window, (255, 255, 255), (120, 500, 250, 75), 3)
+            self.Window.blit(self.font1.render("1 v 1", True, (255, 255, 255)), [175, 210])
+            self.Window.blit(self.font1.render("4-Player", True, (255, 255, 255)), [135, 310])
+            self.Window.blit(self.font1.render("Options", True, (255, 255, 255)), [145, 410])
+            self.Window.blit(self.font1.render("Quit", True, (255, 255, 255)), [190, 510])
+            self.Window.blit(pygame.image.load("sound.png"), (10,710))
             if option1.collidepoint(mx, my):
                 if Clicked:
                     self.StartGame()
                     meow = False
             if option2.collidepoint(mx, my):
                 if Clicked:
-                    self.mainmenu()
+                    pass
             if option3.collidepoint(mx, my):
+                if Clicked:
+                    self.mainmenu()
+            if option4.collidepoint(mx, my):
                 if Clicked:
                     pygame.quit()
             for event in pygame.event.get():
@@ -41,10 +47,11 @@ class Menu:
 
     def StartGame(self):
         self.Window.fill((255, 255, 255))
-        self.Window.blit(self.font.render("1 V 1", True, (0, 0, 0)), [150, 200])
-        self.Window.blit(self.font3.render("Use your left and right arrows to move", True, (0, 0, 0)), [40, 750])
-        self.Window.blit(self.font2.render("Press[Q] to Quit ", True, (0, 0, 0)), [120, 420])
-        self.Window.blit(self.font2.render("Press[R] to Play ", True, (0, 0, 0)), [120, 370])
+        self.Window.blit(pygame.image.load("Backgrounds.jpg"), (-50, -65))
+        self.Window.blit(self.font.render("1 V 1", True, (255, 255, 255)), [150, 200])
+        self.Window.blit(self.font3.render("Use your left and right arrows to move", True, (255, 255, 255)), [40, 750])
+        self.Window.blit(self.font2.render("Press[Q] to Quit ", True, (255, 255, 255)), [120, 420])
+        self.Window.blit(self.font2.render("Press[R] to Play ", True, (255, 255, 255)), [120, 370])
         pygame.display.flip()
         check = True
         while check:
@@ -52,15 +59,17 @@ class Menu:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_q:
-                        pygame.quit()
                     if event.key == pygame.K_r:
                         check = False
+                    if event.key == pygame.K_q:
+                        self.Window.blit(pygame.image.load("Backgrounds.jpg"), (-50, -65))
+                        self.mainmenu()
+
 
     def pause(self):
-        self.Window.blit(self.font1.render("Game Paused", True, (0, 0, 0)), [80, 300])
-        self.Window.blit(self.font2.render("Press[Q] to Quit ", True, (0, 0, 0)), [120, 420])
-        self.Window.blit(self.font2.render("Press[R] to Continue ", True, (0, 0, 0)), [100, 370])
+        self.Window.blit(self.font1.render("Game Paused", True, (255, 255, 255)), [80, 300])
+        self.Window.blit(self.font2.render("Press[Q] to Quit ", True, (255, 255, 255)), [120, 420])
+        self.Window.blit(self.font2.render("Press[R] to Continue ", True, (255, 255, 255)), [100, 370])
         pygame.display.flip()
         pause = True
         while pause:
