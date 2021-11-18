6ddf946f7a9a4297b2f6f5f87ba1d90040362560
ShahriarMiah <85572689+ShahriarMiah@users.noreply.github.com>
2021-11-18 16:52:15 +0000

    Update Ball.py

--- Ball.py
+++ Ball.py
@@ -37,51 +37,51 @@ class BallClass:
         from main import Player2
         if self.Y >= 720:
             if Player1.X < self.X < Player1.X + 10 or Player1.X < self.X + 32 < Player1.X + 10:
-                self.angle = -135
+                self.angle = -130
             if Player1.X + 10 < self.X < Player1.X + 20 or Player1.X + 10 < self.X + 32 < Player1.X + 20:
-                self.angle = -150
+                self.angle = -145
             if Player1.X + 20 < self.X < Player1.X + 40 or Player1.X + 20 < self.X + 32 < Player1.X + 40:
-                self.angle = -165
+                self.angle = -160
             if Player1.X + 40 < self.X < Player1.X + 60 or Player1.X + 40 < self.X + 32 < Player1.X + 60:
-                self.angle = 170
+                self.angle = 160
             if Player1.X + 60 < self.X < Player1.X + 80 or Player1.X + 60 < self.X + 32 < Player1.X + 80:
-                self.angle = 190
+                self.angle = 180
             if Player1.X + 100 < self.X < Player1.X + 110 or Player1.X + 100 < self.X + 32 < Player1.X + 110:
-                self.angle = 165
+                self.angle = 175
             if Player1.X + 110 < self.X < Player1.X + 128 or Player1.X + 110 < self.X + 32 < Player1.X + 128:
-                self.angle = 150
+                self.angle = 160
         elif self.Y <= 30:
             if Player2.X < self.X < Player2.X + 10 or Player2.X < self.X + 32 < Player2.X + 10:
                 if self.check == "Left":
-                    self.angle = 45
+                    self.angle = 55
                 else:
-                    self.angle = -45
+                    self.angle = -55
             if Player2.X + 10 < self.X < Player2.X + 20 or Player2.X + 10 < self.X + 32 < Player2.X + 20:
                 if self.check == "Left":
-                    self.angle = 30
+                    self.angle = 40
                 else:
-                    self.angle = -30
+                    self.angle = -40
             if Player2.X + 20 < self.X < Player2.X + 40 or Player2.X + 20 < self.X + 32 < Player2.X + 40:
                 if self.check == "Left":
-                    self.angle = 15
+                    self.angle = 25
                 else:
-                    self.angle = -15
+                    self.angle = -25
             if Player2.X + 40 < self.X < Player2.X + 60 or Player2.X + 40 < self.X + 32 < Player2.X + 60:
                 if self.check == "Left":
-                    self.angle = 10
+                    self.angle = 20
                 else:
-                    self.angle = -10
+                    self.angle = -20
             if Player2.X + 60 < self.X < Player2.X + 80 or Player2.X + 60 < self.X + 32 < Player2.X + 80:
                 if self.check == "Left":
-                    self.angle = 10
+                    self.angle = 20
                 else:
-                    self.angle = -10
+                    self.angle = -20
             if Player2.X + 80 < self.X < Player2.X + 100 or Player2.X + 80 < self.X + 32 < Player2.X + 100:
                 if self.check == "Left":
-                    self.angle = 15
+                    self.angle = 25
 
             if Player2.X + 100 < self.X < Player2.X + 128 or Player2.X + 100 < self.X + 32 < Player2.X + 128:
                 if self.check == "Left":
-                    self.angle = 30
+                    self.angle = 40
                 else:
-                    self.angle = -30
+                    self.angle = -40
