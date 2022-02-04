2ae7b9af10f19166ca2f6ac7d925cddfc9ec30f8
ShahriarMiah <85572689+ShahriarMiah@users.noreply.github.com>
2022-02-04 22:14:07 +0000

    Update Ball.py

--- Ball.py
+++ Ball.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 import math
 
@@ -26,6 +28,7 @@ class BallClass:
         return newX, newY
 
     def ballmovement(self, Window, p1, p2):
+        from Powers import particle1
         Window.blit(pygame.image.load("ball.png"), (self.X, self.Y))
         # Calculating the speed of the ball dependent upon the angle
         # This will change once the ball hits the paddle
@@ -37,13 +40,21 @@ class BallClass:
         if self.X <= p1:
             self.check = "Left"
             self.angle = -1 * self.angle
+            particle1.particles = []
+            particle1.update = "Circles"
         elif self.X + 32 >= p2:
             self.angle = -1 * self.angle
             self.check = "Right"
+            particle1.particles = []
+            particle1.update = "Rectangles"
         elif self.Y <= 0:
             self.angle = 180 - self.angle
+            particle1.particles = []
+            particle1.update = "Nyan"
         elif self.Y + 32 >= 780:
             self.angle = 180 - self.angle
+            particle1.particles = []
+            particle1.update = "Circles"
 
     def playerCollision(self, Player1, Player2):
 
@@ -103,4 +114,60 @@ class BallClass:
             if Player2.Length == 200:
                 if Player2.X + 128 < self.X < Player2.X + 200 or Player2.X + 128 < self.X < Player2.X + 200:
                     self.angle = 40
+
+    def magnetmovement(self, Player1, ball):
+        if ball == "B3":
+            from AI import blockAI
+        if Player1.Y > 500:
+
+            if self.Y > 400:
+                dx, dy = (Player1.X - self.X, Player1.Y - self.Y)
+                stepx, stepy = (dx / 30, dy / 30)
+                self.X += stepx
+                self.Y -= stepy
+                if Player1.X < self.X < Player1.X + Player1.Width:
+                    if self.Y >= 720:
+                        blockAI.magnet = False
+                        blockAI.checkHits = 0
+                        blockAI.player = random.randint(0, 1)
+                        blockAI.Choice = random.randint(0, 4)
+        else:
+            if self.Y < 380:
+                dx, dy = (Player1.X - self.X, Player1.Y - self.Y)
+                stepx, stepy = (dx / 60, dy / 60)
+                self.X += stepx
+                self.Y += stepy
+                if Player1.X < self.X < Player1.X + Player1.Width:
+                    if self.Y <= 30:
+                        blockAI.magnet = False
+                        blockAI.checkHits = 0
+                        blockAI.player = random.randint(0, 1)
+                        blockAI.Choice = random.randint(0, 4)
+
+        if self.X <= 0:
+            self.angle = -1 * self.angle
+        elif self.X + 10 >= 500:
+            self.angle = -1 * self.angle
+        elif self.Y <= 0:
+            self.angle = 180 - self.angle
+        elif self.Y + 10 >= 700:
+            self.angle = 180 - self.angle
+
+    def get_direction(self, ponglist, new_coordinates_x, new_coordinates_y):
+        vec_list = []
+
+        for i in range(len(ponglist) - 1):
+            x1, y1 = ponglist[i]
+            x2, y2 = ponglist[i + 1]
+            vec = [x2 - x1, y2 - y1]
+            vec_list.append(vec)
+
+        while 0 <= new_coordinates_x <= 500 and 0 <= new_coordinates_y <= 780:
+            new_coordinates_y += vec_list[0][1]
+            new_coordinates_x += vec_list[0][0]
+        return new_coordinates_x
+
+    def draw_line(self, coordinates, Window):
+        pygame.draw.line(Window, (255, 255, 255), (self.X + 16, self.Y + 16), coordinates[0])
+
 
