9d0cf7cd77333276a9333c0e43ee4526a03dac46
ShahriarMiah <85572689+ShahriarMiah@users.noreply.github.com>
2022-02-26 09:19:03 +0000

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -8,35 +8,79 @@ class Menu:
         self.font1 = font1
         self.font2 = font2
         self.font3 = font3
-
+        self.difficulty = "Easy"
+        self.mode = "Main"
     def mainmenu(self):
         meow = True
         Clicked = False
+        check = False
+        counter = 0
+
         while meow:
-            self.Window.blit(self.font1.render("4 Player Pong!", True, (255, 255, 255)), [50, 50])
+            self.Window.blit(pygame.image.load("Backgrounds.jpg"), (0, 0))
+            self.Window.blit(self.font1.render("4 Player Pong!", True, (255, 255, 255)), [280, 50])
             mx, my = pygame.mouse.get_pos()
-            option1 = pygame.draw.rect(self.Window, (255, 255, 255), (120, 200, 250, 75), 3)
-            option2 = pygame.draw.rect(self.Window, (255, 255, 255), (120, 300, 250, 75), 3)
-            option3 = pygame.draw.rect(self.Window, (255, 255, 255), (120, 400, 250, 75), 3)
-            option4 = pygame.draw.rect(self.Window, (255, 255, 255), (120, 500, 250, 75), 3)
-            self.Window.blit(self.font1.render("1 v 1", True, (255, 255, 255)), [175, 210])
-            self.Window.blit(self.font1.render("4-Player", True, (255, 255, 255)), [135, 310])
-            self.Window.blit(self.font1.render("Options", True, (255, 255, 255)), [145, 410])
-            self.Window.blit(self.font1.render("Quit", True, (255, 255, 255)), [190, 510])
-            self.Window.blit(pygame.image.load("sound.png"), (10,710))
-            if option1.collidepoint(mx, my):
-                if Clicked:
-                    self.StartGame()
-                    meow = False
-            if option2.collidepoint(mx, my):
-                if Clicked:
-                    pass
-            if option3.collidepoint(mx, my):
-                if Clicked:
-                    self.mainmenu()
-            if option4.collidepoint(mx, my):
-                if Clicked:
-                    pygame.quit()
+
+            if not check:
+                option1 = pygame.draw.rect(self.Window, (255, 255, 255), (370, 200, 250, 75), 3)
+                option2 = pygame.draw.rect(self.Window, (255, 255, 255), (370, 300, 250, 75), 3)
+                option3 = pygame.draw.rect(self.Window, (255, 255, 255), (370, 400, 250, 75), 3)
+                option4 = pygame.draw.rect(self.Window, (255, 255, 255), (370, 500, 250, 75), 3)
+                self.Window.blit(self.font1.render("1 v 1", True, (255, 255, 255)), [425, 210])
+                self.Window.blit(self.font1.render("4-Player", True, (255, 255, 255)), [385, 310])
+                self.Window.blit(self.font1.render("Options", True, (255, 255, 255)), [395, 410])
+                self.Window.blit(self.font1.render("Quit", True, (255, 255, 255)), [440, 510])
+                self.Window.blit(pygame.image.load("sound.png"), (10, 710))
+            else:
+                self.Window.blit(pygame.image.load("Backgrounds.jpg"), (0, 0))
+                option5 = pygame.draw.rect(self.Window, (255, 255, 255), (370, 200, 250, 75), 3)
+                option6 = pygame.draw.rect(self.Window, (255, 255, 255), (370, 300, 250, 75), 3)
+                option7 = pygame.draw.rect(self.Window, (255, 255, 255), (370, 400, 250, 75), 3)
+                self.Window.blit(self.font1.render("Easy", True, (255, 255, 255)), [425, 210])
+                self.Window.blit(self.font1.render("Medium", True, (255, 255, 255)), [385, 310])
+                self.Window.blit(self.font1.render("Hard", True, (255, 255, 255)), [395, 410])
+                self.Window.blit(self.font1.render("Quit", True, (255, 255, 255)), [440, 510])
+
+            if not check:
+                if option1.collidepoint(mx, my):
+                    if Clicked:
+                        check = True
+                        Clicked = False
+                        self.mode = "AI"
+                elif option2.collidepoint(mx, my):
+                    if Clicked:
+                        self.mode = "Main"
+                        Clicked = False
+                        meow = False
+                        self.StartGame()
+                elif option3.collidepoint(mx, my):
+                    if Clicked:
+                        pass
+                elif option4.collidepoint(mx, my):
+                    if Clicked:
+                        pygame.quit()
+            else:
+                if option5.collidepoint(mx, my):
+                    if Clicked:
+                        self.difficulty = "Easy"
+                        Clicked = False
+                        check = False
+                        self.mode = "AI"
+                        break
+                elif option6.collidepoint(mx, my):
+                    if Clicked:
+                        Clicked = False
+                        check = False
+                        self.mode = "AI"
+                        self.difficulty = "Medium"
+                        break
+                elif option7.collidepoint(mx, my):
+                    if Clicked:
+                        Clicked = False
+                        check = False
+                        self.mode = "AI"
+                        self.difficulty = "Hard"
+                        break
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -47,11 +91,11 @@ class Menu:
 
     def StartGame(self):
         self.Window.fill((255, 255, 255))
-        self.Window.blit(pygame.image.load("Backgrounds.jpg"), (-50, -65))
-        self.Window.blit(self.font.render("1 V 1", True, (255, 255, 255)), [150, 200])
-        self.Window.blit(self.font3.render("Use your left and right arrows to move", True, (255, 255, 255)), [40, 750])
-        self.Window.blit(self.font2.render("Press[Q] to Quit ", True, (255, 255, 255)), [120, 420])
-        self.Window.blit(self.font2.render("Press[R] to Play ", True, (255, 255, 255)), [120, 370])
+        self.Window.blit(pygame.image.load("Backgrounds.jpg"), (0, 0))
+        self.Window.blit(self.font.render("1 V 1", True, (255, 255, 255)), [400, 200])
+        self.Window.blit(self.font3.render("Use your left and right arrows to move", True, (255, 255, 255)), [290, 750])
+        self.Window.blit(self.font2.render("Press[Q] to Quit ", True, (255, 255, 255)), [370, 420])
+        self.Window.blit(self.font2.render("Press[R] to Play ", True, (255, 255, 255)), [370, 370])
         pygame.display.flip()
         check = True
         while check:
@@ -64,7 +108,6 @@ class Menu:
                     if event.key == pygame.K_q:
                         self.Window.blit(pygame.image.load("Backgrounds.jpg"), (-50, -65))
                         self.mainmenu()
-
 
     def pause(self):
         self.Window.blit(self.font1.render("Game Paused", True, (255, 255, 255)), [80, 300])
@@ -81,3 +124,4 @@ class Menu:
                         pygame.quit()
                     if event.key == pygame.K_r:
                         pause = False
+
